282c99041c459e06890ddb0ebaf3943190236f98
Omar Ali <101205529+OA4K@users.noreply.github.com>
2023-03-27 11:51:38 +0000

    .

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const test = () => {
     console.log('Hi')
 }
+
+const button = document.getElementById("move-button");
+
+button.addEventListener("click", () => {
+  const currentLeft = button.style.left ? parseInt(button.style.left) : 0;
+  button.style.left = `${currentLeft + 1}px`;
+});
